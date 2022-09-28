51e6ac6bf3aada9415aaf7d745a66ee1e0f97685
Nicola Solazzo <solazzo@logixcorp.it>
2022-09-28 16:22:44 +0200

    check email

--- src/app.ts
+++ src/app.ts
@@ -13,12 +13,12 @@ const { log } = console
 const HOST = process.env.HOST || 'localhost';
 const PORT = process.env.PORT || 3000;
 
-app.get('/', (req:any, res: any) => {
-    res.json({
+app.get('/?email=:email', (req:any, res: any) => {
+    return res.json({
         title: 'Typescript',
-        status:  res.status,
-        message: dt.init()
-    })
+        status: res.statusCode,
+        statusSendEmail: dt.sendEmail(req.params.email)
+    });
 })
 
 app.listen(PORT, () => {
--- src/modules/moduletest.ts
+++ src/modules/moduletest.ts
@@ -1,12 +1,22 @@
-'use strict'
+"use strict";
 
 export class Datatest {
-    init() {
-       return this.start()
+  public start() {
+    return "Hello";
+  }
+  public sendEmail(email: string): any {
+		console.log(/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(email))
+		// return /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(email)
+    if (/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(email)) {
+      return {
+        message: "OK",
+        isSend: true,
+      };
+    } else {
+			return {
+				message: "You have entered an invalid email address!",
+				isSend: false,
+			};
     }
-
-    private start() {
-        return 'Hello'
-    }
+  }
 }
-
